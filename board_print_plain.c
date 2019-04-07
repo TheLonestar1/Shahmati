be5e34b4717a61ec2188bdc17a6ab6b884501c3f
TheLonestar1 <43960228+TheLonestar1@users.noreply.github.com>
2019-04-07 17:23:06 +0700

    Add files via upload

---
+++ board_print_plain.c
@@ -0,0 +1,48 @@
+#include"board_print_plain.h"
+
+void printTab(int tab[9][9]){
+	for(int i = 0; i < 9; i++){
+    	printf("%d ", i);
+        for(int j = 0; j < 8; j++){ 
+        	if(i == 0) {
+        		printf("A B C D F E G H");
+        		break;
+			}
+           	else {
+           		printf("%c ", converts(tab, i, j+1));  
+			   }       
+       	}
+       	printf("\n");
+    }
+}
+
+char converts(int tab[9][9], int i, int j){
+	 switch(tab[i][j]){
+        case 0:
+            return 'p';
+        case 1:
+            return 'r';
+        case 2:
+            return 'b';
+        case 3:
+            return 'k';
+        case 4:
+            return 'q';
+        case 5:
+            return 'n';
+        case 6:
+            return 'p';
+        case 7:
+            return 'r';
+        case 9:
+            return 'b';
+        case 10:
+            return 'k';
+        case 11:
+            return 'q';
+        case 12:
+            return 'n';
+        case 8:
+            return ' ';
+       }
+}
--- main.c
+++ main.c
@@ -1,106 +1,37 @@
-#include<stdio.h>
-
-
-
-
-
-int logic(char  tab[9][9],int a,int b, int c, int d){
-	switch(tab[a][b]){
-		case 'p': 
-			if(((a-1 == c) || ((a-2 == c) && (a == 6))) && (b == d)){
-				return 1;
-			}
-			else return 0;
-		case 'r': 
-			if(((a == c) && (d != b)) || ((d == b) && (a != c))){
-				return 1;
-			}
-			else return 0;
-		case 'n': 
-			if((((a + 1) == c) && ((b + 2) == d)) || (((a + 1) == c) && ((b - 2) == d)) || (((a - 1) == c) && ((b + 2) == d)) || (((a - 1) == c) && ((b - 2) == d)) || (((a + 2) == c) && ((b + 1) == d)) || (((a + 2) == c) && ((b - 1) == d)) || (((a - 2) == c) && ((b + 1) == d)) || (((a - 2) == c) && ((b - 1) == d)) ) {
-				return 1;
-			}
-			else return 0;
-		case 'b': 
-			if(((a-c) == (b-d)) && (a != c) || ((a-c) == (d-b)) && (b != d)){
-				return 1;
-			}
-			else return 0;	
-		case 'k': 
-			if( (  (a == c) ||  (d == b) ) || ( ( (a-c) == (b-d)) || ((a-c) == (d-b)) ) && ((a - 2) < c < (a+2) && ((a-2) < d < (b + 2) ) ) ){
-				return 1;
-			}
-			else return 0;	
-		case 'q': 
-			if( (  (a == c) ||  (d == b) ) || ( ( (a-c) == (b-d)) || ((a-c) == (d-b)) ) ){
-				return 1;
-			}
-			else return 0;	
-	}
-	return 0;	
-}
-
-void Hod(char tab[9][9],int a,int b,int c,int d,int *count){
-    if(logic(tab,a,b,c,d) == 1){
-		char temp = tab[a][b];
-    	tab[a][b] = tab[c][d];
-    	tab[c][d] = temp;
-	}
-}
-int convert(char word){
-    switch(word){
-        case 'A':
-            return 1;
-        case 'B':
-            return 2;
-        case 'C':
-            return 3;
-        case 'D':
-            return 4;
-        case 'F':
-            return 5;
-        case 'E':
-            return 6;
-        case 'G':
-            return 7;
-        case 'H':
-            return 8;
-       }
-    return 0;
-}
+#include"chess.h"
+#include"board_print_plain.h"
 
 int main(){
 
-	int counter_all[7] = {0};
+	int shah = 1;
+	int mat = 0;
     int num,num1;
-    char word,word1;
-    char tab[9][9] =
-    {
-    {'/','A','B','C','D','F','E','G','H'},
-    {'1','r','n','b','q','k','b','n','r'},
-    {'2','p','p','p','p','p','p','p','p'},
-    {'3',' ',' ',' ',' ',' ',' ',' ',' '},
-    {'4',' ',' ',' ',' ',' ',' ',' ',' '},
-    {'5',' ',' ',' ',' ',' ',' ',' ',' '},
-    {'6',' ',' ',' ',' ',' ',' ',' ',' '},
-    {'7','p','p','p','p','p','p','p','p'},
-    {'8','r','n','b','q','k','b','n','r'}
-    };
-    while(1){   
-     for(int i = 0; i < 9; i++){
-     	printf("%c ", tab[i][0]); 
-        for(int j = 0; j < 8; j++){ 
-            printf("%c", tab[i][j+1]);         
-        }
-        printf("\n");
-    }
+    char word,word1,hod,shma;
+	int tab[9][9] = 
+   {
+ {'/','1','2','3','4','5','6','7','8'},
+ {'A',7,12,9,10,11,9,12,7},
+ {'B',6,6,6,6,6,6,6,6},
+ {'C',8,8,8,8,8,8,8,8},
+ {'D',8,8,8,8,8,8,8,8},
+ {'F',8,8,8,8,8,8,8,8},
+ {'E',8,8,8,8,8,8,8,8},
+ {'G',0,0,0,0,0,0,0,0},
+ {'H',1,5,2,3,4,2,5,1},
+};
+    while(mat == 0){   
+    
+    printTab(tab);
     printf("Vvedite vash hod(A4,B6)=");
     
-    scanf("%c%d,%c%d", &word, &num, &word1, &num1); 
+    scanf("%c%d%c%c%d%c", &word, &num,&hod, &word1, &num1, &shma); 
     if((1 <= num ) && ( num <= 8) && (1 <= num1 ) && ( num1 <= 8) && ('A' <= word ) && ( word <= 'H')&& ('A' <= word1 ) && ( word1 <= 'H')){
-    	Hod(tab,convert(word),num,convert(word1),num1,counter_all);
+    	Hod(tab,convert(word),num,convert(word1),num1,hod,shma,mat);
 	} else {
 		printf("Vvedeno nepravilno... Vvedite eshe raz\n");
+	}
+	if(mat == 1) {
+		printf("you win!!!\n");
 	}
 }
  
