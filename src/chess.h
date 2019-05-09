c6f8935a4916ea453f42c3ff6ebe81e50ebe9e05
TheLonestar1 <43960228+TheLonestar1@users.noreply.github.com>
2019-05-09 22:01:49 +0700

    Update chess.h

--- src/chess.h
+++ src/chess.h
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 #endif
 int convert(char word);
-void Hod(int tab[9][9],int a,int b,int c,int d,char hod,char shma,int *mat);
+int Hod(int tab[9][9],int a,int b,int c,int d,char hod,char shma,int *mat);
 int logic(int tab[9][9],int a,int b, int c, int d);
 int logic_trough(int tab[9][9],int a, int b, int c,int d,char hod);
 int logic_mat(int tab[9][9],int a, int b, int c,int d,char shma, int *mat);
