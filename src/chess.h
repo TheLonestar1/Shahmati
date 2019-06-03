340ad1294da413052e564131c104c7a3e8cb7961
Юрьев Юрий Вадимович <is842s21@mwp12.csc.local>
2019-06-03 11:24:26 +0700

    Change src/

--- src/chess.h
+++ src/chess.h
@@ -7,7 +7,4 @@ int convert(char word);
 int Hod(int tab[9][9],int a,int b,int c,int d,char hod);
 int logic(int tab[9][9],int a,int b, int c, int d);
 int logic_trough(int tab[9][9],int a, int b, int c,int d,char hod);
-int logic_mat(int tab[9][9],int a, int b, int c,int d,char shma, int *mat);
 int logic_kill(int tab[9][9],int a, int b, int c,int d,char hod);
-int logic_shah(int tab[9][9],int a, int b, int c,int d,char shma);
-void logic_king(int tab[9][9],int a, int b, int *mat);
